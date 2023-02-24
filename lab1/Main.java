5c7d3f329ed3938ccf8a4554651fbc3b193296d8
Ilya Kuznetsov <78645533+EuphoriaV@users.noreply.github.com>
2023-02-25 00:31:01 +0300

    Add files via upload

--- lab1/Main.java
+++ lab1/Main.java
@@ -7,9 +7,12 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Function function = new Sum(List.of(new Pow(new Variable("x"), new Const(2.0)),
-                new Multiply(new Const(2.0), new Pow(new Variable("y"), new Const(2.0))),
-                new Exp(new Add(new Variable("x"), new Variable("y")))));
+        Function function = new Add(new Multiply(new Const(100.0),new Pow(new Subtract(new Variable("y"), new Pow(new Variable("x"), new Const(2.0))),new Const(2.0))),
+                new Pow(new Subtract(new Const(1.0), new Variable("x")), new Const(2.0)));
+        Function function1 = new Multiply(
+                new Sin(new Sum(List.of(new Multiply(new Const(0.5), new Pow(new Variable("x"), new Const(2.0))), new Const(3.0),
+                        new Negate(new Multiply(new Const(0.25), new Pow(new Variable("y"), new Const(2.0))))))),
+                new Cos(new Sum(List.of(new Multiply(new Const(2.0), new Variable("x")), new Const(1.0), new Negate( new Exp(new Variable("y")))))));
         Map<String, Double> minimum = FunctionUtils.gradientDescent(function);
         System.out.println(minimum);
         System.out.println(function.evaluate(minimum));
